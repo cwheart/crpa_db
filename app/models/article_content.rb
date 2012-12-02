5d7fe9e619335f7d5a9c01b30fa04bdfbd64ed58
heart <xinshuaifeng@gmail.com>
2012-12-02 21:30:59 +0800

    migrate db content code

--- app/models/article_content.rb
+++ app/models/article_content.rb
@@ -2,7 +2,7 @@ class ArticleContent < ActiveRecord::Base
   # attr_accessible :title, :body
   belongs_to :article
 
-  def migrate_content
+  def self.migrate_content
     Article.find_each do |article|
       next unless File.File.exist?("/www/crpa_ruby/current/txts/#{article.content}")
       content_tmp="";
@@ -13,5 +13,17 @@ class ArticleContent < ActiveRecord::Base
       end
       ArticleContent.create(article_id: article.id, content: content_tmp)
     end
+  end
+
+  def self.test_migrate_content(test_id)
+    article = Article.find(test_id)
+    next unless File.File.exist?("/www/crpa_ruby/current/txts/#{article.content}")
+    content_tmp="";
+    File.open("/www/crpa_ruby/current/txts/#{article.content}","r") do |file|
+      while line = file.gets
+        content_tmp << line
+      end
+    end
+    ArticleContent.create(article_id: article.id, content: content_tmp)
   end
 end
--- app/models/article_content_org.rb
+++ app/models/article_content_org.rb
@@ -2,7 +2,7 @@ class ArticleContentOrg < ActiveRecord::Base
   # attr_accessible :title, :body
   belongs_to :article_org
 
-  def migrate_content
+  def self.migrate_content
     ArticleOrg.find_each do |article|
       next unless File.File.exist?("/www/crpa_ruby/current/txts/#{article.content}")
       content_tmp="";
@@ -13,5 +13,17 @@ class ArticleContentOrg < ActiveRecord::Base
       end
       ArticleOrgContent.create(article_id: article.id, content: content_tmp)
     end
+  end
+
+  def self.test_migrate_content(test_id)
+    article = ArticleOrg.find(test_id)
+    next unless File.File.exist?("/www/crpa_ruby/current/txts/#{article.content}")
+    content_tmp="";
+    File.open("/www/crpa_ruby/current/txts/#{article.content}","r") do |file|
+      while line = file.gets
+        content_tmp << line
+      end
+    end
+    ArticleOrgContent.create(article_id: article.id, content: content_tmp)
   end
 end
